f9140d0ac8aeb37c7cb75b6ec863953f994b15da
Deltafive-Piya <pierstoddard@gmail.com>
2023-07-06 16:06:27 -0400

    Nested Dictionaries & Lists- completed3

--- fundamentals/introduction/nested_dict_and_list.py
+++ fundamentals/introduction/nested_dict_and_list.py
@@ -1,5 +1,5 @@
 #Update Values in Dictionaries and Lists
-print("Update Values in Dictionaries and Lists")#----------------------------------------------------------------------
+print("Update Values in Dictionaries and Lists----------------------------------------------------------------------")
 
 x = [ [5,2,3], [10,8,9]
 ]
@@ -31,6 +31,54 @@ z = [ {'x': 10, 'y': 20}
 print(z[0]['y'])                #Control
 z[0]['y'] = 30                  #Changing the y section in the first entry of z
 print(z[0]['y'])                #Proving it worked
+print('\n')
 
 #Iterate Through a List of Dictionaries
-print("Iterate Through a List of Dictionaries")#----------------------------------------------------------------------
+print("Iterate Through a List of Dictionaries----------------------------------------------------------------------")
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+#Multi Line responses
+print("iterateDictMulti")
+def iterateDictionaryMulti(students):
+    for student in students:
+            for key, value in student.items():
+                print(f'{key} - {value}')
+iterateDictionaryMulti(students)
+print('\n')
+
+
+#Single Line responses WITH COMMAS
+print("iterateDictSingle")
+def iterateDictionarySingle(students):
+    for student in students:
+        for key, value in student.items():
+            print(f'{key} - {value}', end=', ')
+        print()  # Print a newline after each student's names
+iterateDictionarySingle(students)
+print('\n')
+
+# Get Values From a List of Dictionaries
+print("Get Values From a List of Dictionaries----------------------------------------------------------------------")
+def iterateDictionary2(key_name, students):
+    for item in students:
+        if key_name in item:
+            print(item[key_name])
+
+print("Selected first names:")
+iterateDictionary2('first_name', students)
+print('\n')
+print("Selected last names:")
+iterateDictionary2('last_name', students)
+print('\n')
+
