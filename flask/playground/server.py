cc35f14baabfa5feb0d0e74d6d528fcf3b02a460
Deltafive-Piya <pierstoddard@gmail.com>
2023-07-07 02:41:49 -0400

    flask-Playground-complete_lvl_2

--- flask/playground/server.py
+++ flask/playground/server.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Welcome to the Playground!'
+
+@app.route('/play/<int:x>')                     #Adjustment from /play to /play/num_goes_here
+def play(x):
+    return render_template('play.html', x=x)    #Redundant Assignment
 
 if __name__ == '__main__':
     app.run()
