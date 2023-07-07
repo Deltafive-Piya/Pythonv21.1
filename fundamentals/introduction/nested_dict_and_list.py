ae776d81b6a3a105eaf37ca42cab67acedbe2416
Deltafive-Piya <pierstoddard@gmail.com>
2023-07-06 21:13:30 -0400

    Nested Dictionaries & Lists- completed_all

--- fundamentals/introduction/nested_dict_and_list.py
+++ fundamentals/introduction/nested_dict_and_list.py
@@ -82,3 +82,18 @@ print("Selected last names:")
 iterateDictionary2('last_name', students)
 print('\n')
 
+# Iterate Through a Dictionary with List Values
+print("Iterate Through a Dictionary with List Values----------------------------------------------------------------------")
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def printInfo(dojo):
+    for key, value in dojo.items():
+        print(f'{len(value)} {key}')
+        for item in value:
+            print(item)
+        print()
+printInfo(dojo)
