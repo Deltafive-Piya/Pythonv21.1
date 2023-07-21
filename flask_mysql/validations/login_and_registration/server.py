0bec073bba45be37d1bc08224e0fbb9e88dac5c2
Deltafive-Piya <pierstoddard@gmail.com>
2023-07-21 12:39:22 -0400

    create_working-headshops_and_registration

--- flask_mysql/validations/login_and_registration/server.py
+++ flask_mysql/validations/login_and_registration/server.py
@@ -1,5 +1,5 @@
 from flask_app import app #import app
-from flask_app.controllers import controller_routes, controller_headshop
+from flask_app.controllers import controller_routes, controller_headshop #this line targets and imports all controller files
 
 if __name__ == '__main__':
     app.run(debug=True)
