ceb022f28e4d8eaa998a8d1f8bad3669bdc7886b
Deltafive-Piya <pierstoddard@gmail.com>
2023-07-24 03:22:26 -0400

    complete-recipes

--- flask_mysql/validations/login_and_registration/Market_and_Vendors/flask_app/__init__.py
+++ flask_mysql/validations/login_and_registration/Market_and_Vendors/flask_app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.secret_key = 'shhhhhh......'
+app.secret_key = 'lssa secret'
 bcrypt = Bcrypt(app)
 
 DATABASE = 'markets_db'
